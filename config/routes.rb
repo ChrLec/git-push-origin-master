e6d5b307b19147f49ca9214fd1edb45f599630ae
Christelle Lecamp <christelle.lecamp@gmail.com>
2021-12-11 16:04:45 +0100

    exo minimal done dans la douleur

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Create one task
+  get "/tasks/new", to: "tasks#new"
+
   # Read all
   get "/tasks", to: "tasks#index"
+  post "/tasks", to: "tasks#create"
 
   # Read one task
-  get "/tasks/:id", to: "tasks#show", as: :show_restaurant
+  get "/tasks/:id", to: "tasks#show", as: :task
+  patch "/tasks/:id", to: "tasks#update"
 
+  # Update task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  delete "/tasks/:id", to: "tasks#destroy", as: :delete_task
 end
